6e611b991c5f4b4ec9fc4a57132b9e07303ff588
ztranya <132863747+ztranya@users.noreply.github.com>
2025-11-30 17:45:41 -0500

    Update Etudiant.java

--- rest-api/src/main/java/com/diro/ift2255/model/Etudiant.java
+++ rest-api/src/main/java/com/diro/ift2255/model/Etudiant.java
@@ -9,122 +9,74 @@ public class Etudiant {
     private String programme;
     private int cycle;
     private LocalDateTime debutProgramme;
-    private Preferences preferences;
-    private sigle[] coursPasse;
+    private Preferences preferences;   // enum
+    private String[] coursPasse;       // sigles des cours passés
     private String[] filtres;
 
+    public Etudiant() {}
 
-
-    public void setmatricule(int matricule){
-
+    // Getters / Setters
+    public void setMatricule(int matricule){
         this.matricule = matricule;
     }
-
     public int getMatricule(){
-
         return matricule;
     }
 
-
     public void setPrenom(String prenom){
-
         this.prenom = prenom;
     }
-
     public String getPrenom(){
-
         return prenom;
     }
 
     public void setNom(String nom){
-
         this.nom = nom;
     }
-
     public String getNom(){
-
         return nom;
     }
 
     public void setProgramme(String programme){
-
         this.programme = programme;
     }
-
     public String getProgramme(){
-
         return programme;
     }
 
     public void setCycle(int cycle){
-
         this.cycle = cycle;
     }
-
     public int getCycle(){
-
         return cycle;
     }
 
     public void setDebutProgramme(LocalDateTime debutProgramme){
-
         this.debutProgramme = debutProgramme;
     }
-
     public LocalDateTime getDebutProgramme(){
-
         return debutProgramme;
     }
 
     public void setPreferences(Preferences preferences){
-
-    this.preferences = preferences;
-
+        this.preferences = preferences;
     }
-
     public Preferences getPreferences(){
-
         return preferences;
     }
 
-    public void setCoursPasse(sigle[] coursPasse){
-
+    public void setCoursPasse(String[] coursPasse){
         this.coursPasse = coursPasse;
     }
-
-    public sigle[] getcoursPasse(){
-
+    public String[] getCoursPasse(){
         return coursPasse;
     }
 
-
     public void setFiltres(String[] filtres){
-
         this.filtres = filtres;
     }
-
     public String[] getFiltres(){
-
         return filtres;
-    }
-
-
-    public void modifierProfil(){
-
-
-
-    }
-
-
-    public void ajouterFiltre(){
-
-
-    }
-
-
-    public void sauvegarderSelection(){
-
-        
     }
 }
 
