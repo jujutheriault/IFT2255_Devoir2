4a39ece3ff8bde946a50fcca68c8652675fe7cc7
ztranya <132863747+ztranya@users.noreply.github.com>
2025-11-30 17:44:02 -0500

    Update TableauComparaison.java

--- rest-api/src/main/java/com/diro/ift2255/model/TableauComparaison.java
+++ rest-api/src/main/java/com/diro/ift2255/model/TableauComparaison.java
@@ -5,31 +5,31 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class TableauComparaison {
 
-    private Cours[] cours;
+    private Course[] cours;
     private int taille;
 
     public TableauComparaison() {
-        this.cours = new Cours[10]; // capacité par défaut
+        this.cours = new Course[10]; // capacité par défaut
         this.taille = 0;
     }
 
     public TableauComparaison(int capacite) {
-        this.cours = new Cours[capacite];
+        this.cours = new Course[capacite];
         this.taille = 0;
     }
 
     // --- Getter ---
-    public Cours[] getCours() {
+    public Course[] getCours() {
         return cours;
     }
 
-    public void setCours(Cours[] cours) {
+    public void setCours(Course[] cours) {
         this.cours = cours;
-        this.taille = cours.length;
+        this.taille = (cours == null) ? 0 : cours.length;
     }
 
-    //Ajouter un cours
-    public void ajouterCours(Cours c) {
+    // Ajouter un cours
+    public void ajouterCours(Course c) {
         if (taille >= cours.length) {
             agrandirTableau();
         }
@@ -38,19 +38,19 @@ public class TableauComparaison {
     }
 
     private void agrandirTableau() {
-        Cours[] nouveau = new Cours[cours.length * 2];
+        Course[] nouveau = new Course[cours.length * 2];
         for (int i = 0; i < cours.length; i++) {
             nouveau[i] = cours[i];
         }
         cours = nouveau;
     }
 
-    //Calculer la charge totale
+    // Calculer la charge totale (somme des crédits)
     public int calculChargeTotale() {
         int total = 0;
         for (int i = 0; i < taille; i++) {
             if (cours[i] != null) {
-                total += cours[i].getCredits(); 
+                total += cours[i].getCredits();
             }
         }
         return total;
@@ -60,4 +60,3 @@ public class TableauComparaison {
         return taille;
     }
 }
-
